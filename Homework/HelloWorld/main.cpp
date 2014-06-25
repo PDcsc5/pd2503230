5184ec3a71a14c52184ea90f32e1b51e3e376380
pdcsc5 <pdelacruz5@student.rcc.edu>
2014-06-25 13:24:41 -0700

    Signed-off-by: pdcsc5 <pdelacruz5@student.rcc.edu>

---
+++ Homework/HelloWorld/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Patrick De La Cruz
+ * Created on June 25, 2014, 1:15 PM
+ * First Program
+ */
+
+//system Level Libraries
+#include <iostream>
+using namespace std;
+
+//user libraries
+
+//Global constant
+
+//Function prototype
+
+//Execution begins
+int main(int argc, char** argv) {
+    //output simple text
+    cout<<"Hello World"<<endl;
+    //Exit stage right!
+    return 0;
+}
+
