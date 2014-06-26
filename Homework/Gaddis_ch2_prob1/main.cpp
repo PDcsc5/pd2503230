dcdfd1e4db353f0b531f81a55879b54635ba8b1b
pdcsc5 <pdelacruz5@student.rcc.edu>
2014-06-26 12:37:09 -0700

    Signed-off-by: pdcsc5 <pdelacruz5@student.rcc.edu>

---
+++ Homework/Gaddis_ch2_prob1/main.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   main.cpp
+ * Author: Patrick De La Cruz
+ * Created on June 26, 2014, 12:00 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+//User Libraries
+
+//Global Constants
+
+//Execution Begins Here
+int main(int argc, char** argv) {
+    
+    int x=62,y=99,total=x+y;
+      
+     cout<<total<<endl;
+    
+    
+    return 0;
+}
+
